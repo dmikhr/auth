cc4169da94276db36c3e6666ab4eed773732d702
Dmitrii Khramtsov <dmitrypkh@gmail.com>
2024-10-11 10:08:02 +0300

    fixes

--- cmd/main.go
+++ cmd/main.go
@@ -6,37 +6,17 @@ import (
 	"log"
 	"net"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	desc "github.com/dmikhr/auth/pkg/user_v1"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
 const grpcPort = 50051
 
 type server struct {
 	desc.UnimplementedAuthV1Server
-}
-
-func (s *server) Create(_ context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
-	log.Printf("User data: %s | %s | %s | %s | %v", req.Name, req.Email, req.Password, req.PasswordConfirm, req.Role)
-	return &desc.CreateResponse{}, nil
-}
-
-func (s *server) Get(_ context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
-	log.Printf("Get id: %d", req.Id)
-	return &desc.GetResponse{}, nil
-}
-
-func (s *server) Update(_ context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
-	log.Printf("Update data: %d | %s | %s | %v", req.Id, req.Name, req.Email, req.Role)
-	return &emptypb.Empty{}, nil
-}
-
-func (s *server) Delete(_ context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
-	log.Printf("Chat to delete id: %d", req.Id)
-	return &emptypb.Empty{}, nil
 }
 
 func main() {
@@ -54,4 +34,28 @@ func main() {
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+// Create - создание нового пользователя
+func (s *server) Create(_ context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("User data: %s | %s | %s | %s | %v", req.GetName(), req.GetEmail(), req.GetPassword(), req.GetPasswordConfirm(), req.GetRole())
+	return &desc.CreateResponse{}, nil
+}
+
+// Get - получить пользователя по id
+func (s *server) Get(_ context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("Get id: %d", req.GetId())
+	return &desc.GetResponse{}, nil
+}
+
+// Update - обновление данных пользователя
+func (s *server) Update(_ context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	log.Printf("Update data: %d | %s | %s | %v", req.GetId(), req.GetName(), req.GetEmail(), req.GetRole())
+	return &emptypb.Empty{}, nil
+}
+
+// Delete - удаление пользователя
+func (s *server) Delete(_ context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Chat to delete id: %d", req.GetId())
+	return &emptypb.Empty{}, nil
 }
